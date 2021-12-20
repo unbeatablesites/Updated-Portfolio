328500d2d8a25658f7db95e4802361e956075b82
Frank <Frank@Franks-Mac-mini.local>
2021-12-19 23:53:05 -0500

    Starting Project

--- my-web-portfolio-frank-uzoka/src/App.js
+++ my-web-portfolio-frank-uzoka/src/App.js
@@ -1,19 +1,17 @@
-import logo from './logo.svg';
+import logo from './logo.jpeg';
 import './App.css';
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Happy New Year!!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="http://frankuzoka.com"
           target="_blank"
-          rel="noopener noreferrer"
         >
           Learn React
         </a>
